5d2c9db4e62d175dcdcab4a188a5434d5f9e8b0a
Hikimory <nikita.bikon@gmail.com>
2023-03-05 21:06:34 +0200

    Create PackingList Controller

--- PackIT.Api/Program.cs
+++ PackIT.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using PackIT.Application;
 using PackIT.Infrastructure;
 
@@ -8,15 +9,24 @@ builder.Services.AddInfrastructure(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PackIT.Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PackIT.Api v1"));
+}
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
